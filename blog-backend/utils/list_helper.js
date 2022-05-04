a69eaf3ca9a2bfe4420087daa592c22b588f6d85
Joel Moran <joelbmoran@gmail.com>
2022-05-04 13:22:59 +0700

    Add and test helper function to find total likes of all blogs (4.4)

--- blog-backend/utils/list_helper.js
+++ blog-backend/utils/list_helper.js
@@ -1,7 +1,15 @@
-const dummy = (blogs) => {
-  return 1;
+const dummy = (a) => {
+  if (a || !a) return 1;
+};
+
+const totalLikes = (blogArrayOfObjects) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes;
+  };
+
+  return blogArrayOfObjects.reduce(reducer, 0);
 };
   
 module.exports = {
-  dummy
+  dummy, totalLikes
 };
