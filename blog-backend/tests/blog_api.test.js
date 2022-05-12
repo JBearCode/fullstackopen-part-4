ba0643980381ebaaa4ce51b6eb045bd46d12a935
Joel Moran <joelbmoran@gmail.com>
2022-05-12 10:27:41 +0700

    Add functionality to delete one resource using RESTful conventions and async/await, then implement test (4.13)

--- blog-backend/tests/blog_api.test.js
+++ blog-backend/tests/blog_api.test.js
@@ -16,106 +16,144 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 }, 100000);
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-}, 100000);
+describe('testing GET requests to base URL', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  }, 100000);
 
-test('there are five blogs', async () => {
-  const response = await api.get('/api/blogs');
-  
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
-}, 100000);
-  
-test('blogs contain an expected title', async () => {
-  const response = await api.get('/api/blogs');
-  const titles = response.body.map(r => r.title);
-  expect(titles).toContain(
-    'another one'
-  );
-}, 100000);
-
-test('blogs contain an expected author', async () => {
-  const response = await api.get('/api/blogs');
-  const authors = response.body.map(r => r.author);
-  expect(authors).toContain(
-    'some dude'
-  );
-}, 100000);
-
-test('blogs have id key', async () => {
-  const response = await api.get('/api/blogs');
-  expect(response.body[0].id).toBeDefined();
-}, 100000);
-
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'learning to use async/await',
-    author: 'myself',
-    url: 'a link',
-    likes: 5
-  };
-  
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+  test('there are five blogs', async () => {
+    const response = await api.get('/api/blogs');
     
-  const titles = blogsAtEnd.map(r => r.title);
-  
-  expect(titles).toContain(
-    'learning to use async/await'
-  );
-}, 100000);
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  }, 100000);
 
-test('authorless / no url blogs are not added', async () => {
-  const authorlessBlog = {
-    title: 'learning to use async/await',
-    url: 'a link',
-    likes: 5
-  };
-  const noUrlBlog = {
-    title: 'learning to use async/await',
-    author: 'somebody',
-    likes: 0
-  };
-  
-  await api
-    .post('/api/blogs')
-    .send(authorlessBlog)
-    .expect(400);
+  test('blogs contain an expected title', async () => {
+    const response = await api.get('/api/blogs');
+    const titles = response.body.map(r => r.title);
+    expect(titles).toContain(
+      'another one'
+    );
+  }, 100000);
 
-  await api
-    .post('/api/blogs')
-    .send(noUrlBlog)
-    .expect(400);
-  
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);  
-}, 100000);
+  test('blogs contain an expected author', async () => {
+    const response = await api.get('/api/blogs');
+    const authors = response.body.map(r => r.author);
+    expect(authors).toContain(
+      'some dude'
+    );
+  }, 100000);
 
-test('likes defaults to 0', async () => {
-  const newBlog = {
-    title: 'checking POST without likes',
-    author: 'me',
-    url: 'a link'
-  };
+  test('blogs have id key', async () => {
+    const response = await api.get('/api/blogs');
+    expect(response.body[0].id).toBeDefined();
+  }, 100000);
+
+});
+
+describe('testing PUT requests', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'learning to use async/await',
+      author: 'myself',
+      url: 'a link',
+      likes: 5
+    };
+        
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+      
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+          
+    const titles = blogsAtEnd.map(r => r.title);
+        
+    expect(titles).toContain(
+      'learning to use async/await'
+    );
+  }, 100000);
+
+  test('authorless / no url blogs are not added', async () => {
+    const authorlessBlog = {
+      title: 'learning to use async/await',
+      url: 'a link',
+      likes: 5
+    };
+    const noUrlBlog = {
+      title: 'learning to use async/await',
+      author: 'somebody',
+      likes: 0
+    };
     
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201);
+    await api
+      .post('/api/blogs')
+      .send(authorlessBlog)
+      .expect(400);
+  
+    await api
+      .post('/api/blogs')
+      .send(noUrlBlog)
+      .expect(400);
     
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
-  expect(blogsAtEnd[blogsAtEnd.length - 1].likes).toEqual(0);
-}, 100000);
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);  
+  }, 100000);
+
+  test('likes defaults to 0', async () => {
+    const newBlog = {
+      title: 'checking POST without likes',
+      author: 'me',
+      url: 'a link'
+    };
+      
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201);
+      
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+    expect(blogsAtEnd[blogsAtEnd.length - 1].likes).toEqual(0);
+  }, 100000);
+});
+
+describe('testing DELETE requests', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+  
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204);
+  
+    const blogsAtEnd = await helper.blogsInDb();
+  
+    expect(blogsAtEnd).toHaveLength(
+      helper.initialBlogs.length - 1
+    );
+  
+    const titles = blogsAtEnd.map(r => r.title);
+  
+    expect(titles).not.toContain(blogToDelete.title);
+  }, 100000);
+});
+  
+
+
+
+
+
+
+
+
+
+
+
 
 
 afterAll(() => {
