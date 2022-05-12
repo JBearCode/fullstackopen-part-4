84f21fcad09f9721d31ea66d280e020ff3dc7acb
Joel Moran <joelbmoran@gmail.com>
2022-05-12 15:24:23 +0700

    Implement way to create new users and save password hashes using bcrypt + allow viewing all users with API endpoint (4.15)

--- blog-backend/tests/blog_api.test.js
+++ blog-backend/tests/blog_api.test.js
@@ -6,6 +6,8 @@ const supertest = require('supertest');
 const app = require('../app');
 const api = supertest(app);
 const Blog = require('../models/blog');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
 beforeEach(async () => {
   await Blog.deleteMany({});
@@ -163,7 +165,59 @@ describe('testing PUT requests', () => {
   }, 100000);
 });
 
+describe('testing users in database', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
 
+    const passwordHash = await bcrypt.hash('sekret', 10);
+    const user = new User({ username: 'root', passwordHash });
+
+    await user.save();
+  });
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'elonmusk',
+      name: 'Elon Musk',
+      password: 'tesla',
+    };
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map(u => u.username);
+    expect(usernames).toContain(newUser.username);
+  });
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'root',
+      name: 'The Dev',
+      password: 'developer',
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    expect(result.body.error).toContain('username must be unique');
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toEqual(usersAtStart);
+  });
+});
 
 
 
