0c49121d126802475cb6b76f0c2ad425ef5bf3d7
Joel Moran <joelbmoran@gmail.com>
2022-05-18 11:34:37 +0700

    Add button to each blog to expand or collapse blog details (5.7)

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,39 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 5,
+    paddingLeft: 5,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleExpansion = () => {
+    expanded ? setExpanded(false) : setExpanded(true)
+  }
+
+  if (!expanded) {
+    return (
+      <div style={blogStyle}>
+        <p>{blog.title} by {blog.author}
+        <button onClick={toggleExpansion}>Expand</button>
+        </p>
+      </div>  
+    ) 
+  } else {
+    return (
+      <div style={blogStyle}>
+        <p>{blog.title}<button onClick={toggleExpansion}>Hide</button></p>
+        <p>Author: {blog.author}</p>
+        <p>URL: {blog.url}</p>
+        <p>Likes: {blog.likes}<button>Like</button></p>
+        <p>Submitted by {blog.user.name}</p>
+      </div>  
+    ) 
+  }
+}
 
 export default Blog
